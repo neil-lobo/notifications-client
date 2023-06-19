1e71be79c3ffceac92f17caae61bc6175488e9f7
Neil Lobo <neil_edward_lobo@hotmail.com>
2023-06-18 21:51:57 -0400

    initial commit

---
+++ lib/index.ts
@@ -0,0 +1,59 @@
+import jsonwebtoken from "jsonwebtoken";
+
+export interface MessageOptions {
+    title?: string;
+    message: string;
+    from?: string;
+    highlighted?: boolean;
+}
+
+export interface JWTPayload {
+    user: string;
+    labels: string[];
+    iss: string;
+    iat: number;
+}
+
+export class NotificationsClient {
+    private token: string;
+    private decodedToken: any;
+    private url: URL | string;
+
+    constructor(url: URL | string, token: string) {
+        this.decodedToken = jsonwebtoken.decode(token);
+        if (!this.decodedToken) {
+            throw new Error("Invalid JWT");
+        }
+
+        this.url = url;
+        this.token = token;
+    }
+
+    getDecodedToken(): JWTPayload {
+        return this.decodedToken;
+    }
+
+    async notify(label: string, options: MessageOptions) {
+        const res = await fetch(new URL("notify", this.url), {
+            method: "POST",
+            headers: {
+                authorization: this.token,
+                "content-type": "application/json",
+            },
+            body: JSON.stringify({
+                label,
+                options,
+            }),
+        });
+        return res;
+    }
+
+    async getConnections() {
+        const res = await fetch(new URL("connections", this.url), {
+            headers: {
+                authorization: this.token,
+            },
+        });
+        return res;
+    }
+}
